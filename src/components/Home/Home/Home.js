1f6fae5013b1fde1838fc0f5e80833df54d3c771
Niloy-Sumon <niloyabidur@gmail.com>
2021-08-28 13:38:19 +0600

    footer done

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -2,11 +2,12 @@ import React from 'react';
 import FeaturedService from '../FeaturedService/FeaturedService';
 import Header from '../Header/Header';
 import Services from '../Services/Services';
-import Appointment from '../Appointment/Appointment'
+import Appointment from '../HomeAppointment/Appointment'
 import Testimonial from '../Testimonial/Testimonial';
 import Blog from '../Blog/Blog';
 import Doctors from '../Doctors/Doctors';
 import Contact from '../Contact/Contact';
+import Footer from '../../Shared/Footer/Footer';
 const Home = () => {
     return (
         <div>
@@ -18,6 +19,7 @@ const Home = () => {
             <Blog></Blog>
             <Doctors></Doctors>
             <Contact></Contact>
+            <Footer></Footer>
         </div>
     );
 };
