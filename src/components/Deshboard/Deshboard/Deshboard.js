c3632da8d9c024a94784343771b0019d098dd359
Niloy-Sumon <niloyabidur@gmail.com>
2021-08-29 21:29:52 +0600

    deshbord some work done

--- src/components/Deshboard/Deshboard/Deshboard.js
+++ src/components/Deshboard/Deshboard/Deshboard.js
@@ -1,8 +1,26 @@
-import React from 'react';
-import DeshboardCalendar from '../DeshboardCalendar/DeshboardCalendar';
+import React, { useEffect, useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import AppointmentByDate from '../AppointmentByDate/AppointmentByDate';
 import Sitebar from '../Sitebar/Sitebar';
 
 const Deshboard = () => {
+    const [seletedDate, setSeletedDate] = useState(new Date())
+    const [appointments, setAppointments] = useState([]);
+
+    const handleOnChange = (date) => {
+        setSeletedDate(date)
+    }
+
+    useEffect(() => {
+        fetch('http://localhost:5000/appointmentsByDate', {
+            method: 'POST',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify({ date: seletedDate })
+        })
+            .then(res => res.json())
+            .then(data => setAppointments(data))
+    }, [seletedDate])
     return (
         <div>
             <div className="container-fluet row">
@@ -10,13 +28,16 @@ const Deshboard = () => {
                     <Sitebar></Sitebar>
                 </div>
                 <div className="col-md-5">
-                    <DeshboardCalendar></DeshboardCalendar>
+                    <Calendar
+                        onChange={handleOnChange}
+                        value={new Date()}
+                    />
                 </div>
-                    <div className="col-md-5">
-
-                    </div>
+                <div className="col-md-5">
+                    <AppointmentByDate appointments={appointments}></AppointmentByDate>
                 </div>
             </div>
+        </div>
     );
 };
 
--- src/components/Deshboard/DeshboardCalendar/DeshboardCalendar.js
+++ src/components/Deshboard/DeshboardCalendar/DeshboardCalendar.js
@@ -1,19 +1,11 @@
-import React, { useState } from 'react';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
-
+import React, { useEffect, useState } from 'react';
 
 const DeshboardCalendar = () => {
-    const [seletedDate, setSeletedDate] = useState(new Date())
-    const handleOnChange = (date) =>{
-        setSeletedDate(date)
-    }
+    
+    
     return (
         <div>
-            <Calendar
-        onChange={handleOnChange}
-        value={new Date()}
-      />
+            
         </div>
     );
 };
